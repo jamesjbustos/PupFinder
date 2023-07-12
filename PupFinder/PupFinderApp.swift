932a114c83d2a1fb5fe990265db71710f04bd9d3
jamesjbustos <45052719+jamesjbustos@users.noreply.github.com>
2023-07-12 12:12:39 -0400

    Initial Commit

---
+++ PupFinder/PupFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  PupFinderApp.swift
+//  PupFinder
+//
+//  Created by James B on 7/12/23.
+//
+
+import SwiftUI
+
+@main
+struct PupFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
